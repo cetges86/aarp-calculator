48af311e7235c9a859aff285a73f4fc81b28e5c5
Christopher Etges <c.etges86@gmail.com>
2018-11-10 10:57:40 -0700

    display prices

--- main.js
+++ main.js
@@ -16,6 +16,10 @@ $(document).ready(function () {
     let selectedArType = document.getElementById("ar-type");
     let selectedProg = document.getElementById("prog-type");
 
+    let framePrice = 0
+    let arPrice = 89
+    let baseLensPrice = 0
+
     selectedLensType.addEventListener("change", function (event) {
         selectedLensType = event.target.value;
         showDropdowns();
@@ -27,6 +31,7 @@ $(document).ready(function () {
             $("#ar-price").show(1000);
         }
     });
+
 
     //displays dropdowns
     function showDropdowns() {
@@ -49,6 +54,51 @@ $(document).ready(function () {
 
     };
     //end of function
+    function displayPrices() {
+        framePrice = $("#frame-price").val();
+        $("#frame-start").text(`Frame Price: $${framePrice}.00`);
+        baseLensPrice = $("#base-lens").find(":selected").data("value");
+        if (selectedLensType === "Progressive" && selectedProg === "Standard Progressive") {
+            baseLensPrice = $('#std-prog-list').find(":selected").data("value");
+
+        } else if (selectedLensType === "Progressive" && selectedProg === "Premium Progressive") {
+            baseLensPrice = parseInt($("#prem-prog-price").val());
+        }
+
+        $("#lens-start").text(`Base Lens Price: $${baseLensPrice}.00`);
+        if (selectedArType === "Premium Anti-Reflective") {
+            arPrice = $("#ar-price").val();
+        }
+        $("#ar-start").text(`Anti-Reflective Price: $${arPrice}.00`);
+    
+        let lensMaterial = $("#lens-material").find(":selected").data("value");
+        $("#material-start").text(`Lens Material Price: $${lensMaterial}.00`);
+        
+        let transChoice = 0
+        if ($('#transitions:checked').val()=== "on"){
+            transChoice = 140
+            console.log("Transitions checked")
+            $('#trans-start').text(`Transitions: $${transChoice}.00`)
+        } else {
+            transChoice = 0
+            $('#trans-start').text(`Transitions: $${transChoice}.00`)
+        }
+
+        let prism = 0
+        if ($('#prism:checked').val()=== "on"){
+            prism = 5*$('#prism-num').val()
+            console.log("Transitions checked")
+            $('#prism-start').text(`Prism: $${prism}.00`)
+        } else if($('#prism:checked').val()=== "off")  {
+            console.log($('#prism:checked').val())
+            prism = 0
+            $('prism-start').text(`Prism: $${prism}.00`)
+        }
+    }
+    
+    $('.input-group').on('change', function(){
+        displayPrices();
+    })
 
     $("#add").on("click", function (event) {
         let newDropdown = ($("#other-options")[0].outerHTML);
@@ -59,24 +109,8 @@ $(document).ready(function () {
 
     $("#calculate").on("click", function () {
         confirm("Are you finished with your order?")
-        let framePrice = $("#frame-price").val();
-        let baseLensPrice = $("#base-lens").find(":selected").data("value");
-        let arPrice = 89
-        let lensMaterial = $("#lens-material").find(":selected").data("value");
-        if (selectedLensType === "Progressive" && selectedProg === "Standard Progressive") {
-            baseLensPrice = $('#std-prog-list').find(":selected").data("value");
 
-        } else if (selectedLensType === "Progressive" && selectedProg === "Premium Progressive") {
-            baseLensPrice = parseInt($("#prem-prog-price").val());
-        }
-
-        if (selectedArType === "Premium Anti-Reflective"){
-            arPrice = $("#ar-price").val();
-
-        }
-
-        
-        console.log(framePrice*.7, baseLensPrice, arPrice*.7, lensMaterial);
+        console.log(framePrice * .7, baseLensPrice, arPrice * .7, lensMaterial);
     });
 
 
